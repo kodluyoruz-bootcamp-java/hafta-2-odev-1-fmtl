a48d44607543610f193981f239b0c94254e47c67
fatmamutluu <45861808+fatmamutluu@users.noreply.github.com>
2020-11-20 23:32:57 +0300

    Add files via upload

---
+++ src/Iphone.java
@@ -0,0 +1,11 @@
+
+public class Iphone extends OrtakOzellikler{
+
+	public void goruntuluGorusme() {
+		System.out.println("Görüntülü görüşme yapılabiliyor");
+	}
+	public void ekranPaylasımı() {
+		System.out.println("Ekran paylasımı yapılabiliyor");
+	}
+	
+}
