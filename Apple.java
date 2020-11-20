0a13f0a7c55f418e8129b480053f5ec05a94d5a2
fatmamutluu <45861808+fatmamutluu@users.noreply.github.com>
2020-11-20 23:37:58 +0300

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,11 @@
+
+public class Apple extends OrtakOzellikler{
+
+	public void goruntuluGorusme() {
+		System.out.println("Görüntülü görüşme yapılabiliyor");
+	}
+	public void ekranPaylasımı() {
+		System.out.println("Ekran paylasımı yapılabiliyor");
+	}
+	
+}
